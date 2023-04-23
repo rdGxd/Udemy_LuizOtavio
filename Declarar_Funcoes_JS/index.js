12addd966c7f1f5316562198ddc1c3c307391e09
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-04-23 15:59:07 -0300

    Functions

---
+++ Declarar_Funcoes_JS/index.js
@@ -0,0 +1,37 @@
+// Declaração de função (Function hoisting)
+function falaOi() {
+  console.log("Oi");
+}
+
+// First-class objects (Objetos de primeira classe)
+// Function expression
+const souUmDado = function () {
+  console.log("Sou um dado.")
+}
+souUmDado();
+/*
+function executaFuncao(funcao) {
+  console.log("Vou executar sua função abaixo");
+  funcao();
+}
+executaFuncao(souUmDado)
+*/
+
+// Arrow function
+const functionArrow = () => {
+  console.log("Sou uma arrow function")
+}
+functionArrow();
+
+// Dentro de um Objeto
+const obj = {
+  /*
+  falar: function() {
+    console.log("Estou falando...")
+  }
+  */
+  falar() {
+    console.log("Estou falando...")
+  }
+};
+obj.falar();
