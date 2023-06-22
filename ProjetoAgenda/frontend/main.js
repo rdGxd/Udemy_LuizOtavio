ec3ac346066e19362b7022edbb1d18f60aaa64b7
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-06-22 12:31:01 -0300

    Validação mínima no frontend

--- ProjetoAgenda/frontend/main.js
+++ ProjetoAgenda/frontend/main.js
@@ -1,3 +1,14 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import "./assets/css/style.css";
+
+import Login from "./modules/Login";
+import Contato from "./modules/Contato";
+
+const login = new Login(".form-login");
+const cadastro = new Login(".form-cadastro");
+const contato = new Contato(".form-contato")
+login.init();
+cadastro.init();
+contato.init();
+
+// import "./assets/css/style.css";
