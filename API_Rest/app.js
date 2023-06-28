6dc1504988d1f0f2c50963f2c84764894cbebf42
Rodrigo Silva <rdg.hora1@gmail.com>
2023-06-28 08:36:24 -0300

    Rotas de alunos

--- API_Rest/app.js
+++ API_Rest/app.js
@@ -9,6 +9,7 @@ import express from "express";
 import homeRoutes from "./src/routes/homeRoutes";
 import userRoutes from "./src/routes/userRoutes";
 import tokenRoutes from "./src/routes/tokenRoutes";
+import alunoRoutes from "./src/routes/alunoRoutes";
 
 class App {
   // Inicializando tudo
@@ -29,6 +30,7 @@ class App {
     this.app.use("/", homeRoutes);
     this.app.use("/users/", userRoutes);
     this.app.use("/tokens/", tokenRoutes);
+    this.app.use("/aluno/", alunoRoutes);
   }
 }
 
--- API_Rest/src/controllers/HomeController.js
+++ API_Rest/src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from "../models/Aluno";
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: "Maria",
-      sobrenome: "Miranda",
-      email: "teste@teste.com",
-      idade: 20,
-      peso: 57,
-      altura: 1.60,
-    });
-    res.json(novoAluno);
+    res.json("HomeIndex");
   }
 }
 
