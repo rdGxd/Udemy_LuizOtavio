95fecabe0952b948dd063353ff6874d7ca57bc66
Rodrigo Silva <rdg.hora1@gmail.com>
2023-06-30 09:18:28 -0300

    Configurações iniciais
    Configurando o primeiro Componente React

---
+++ React_ComponentesClasse(Basic)/lista_tarefas/src/App.js
@@ -0,0 +1,6 @@
+import React from "react"; // Sempre precisa importa o React na hora de criar um componente
+import "./App.css";
+
+export default function App() {
+  return <h1>Hello world!</h1>;
+}
