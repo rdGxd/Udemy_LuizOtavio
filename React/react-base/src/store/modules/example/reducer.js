806ce7ff683fce26c6387ab6cb7a7c0f1f8de16b
Rodrigo Silva <rdg.hora1@gmail.com>
2023-07-04 12:08:11 -0300

    .

--- React/react-base/src/store/modules/example/reducer.js
+++ React/react-base/src/store/modules/example/reducer.js
@@ -11,19 +11,16 @@ const initialState = {
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case types.BOTAO_CLICADO_SUCCESS: {
-      console.log("Sucesso");
       const newState = { ...state };
       newState.botaoClicado = !newState.botaoClicado;
       return newState;
     }
 
     case types.BOTAO_CLICADO_FAILURE: {
-      console.log("Falhamos");
       return state;
     }
 
     case types.BOTAO_CLICADO_REQUEST: {
-      console.log("Estou fazendo a requisição");
       return state;
     }
 
