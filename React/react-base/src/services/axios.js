1ebb64546bed3df58a0842f785a63be9d5fb4aa4
Rodrigo Silva <rdg.hora1@gmail.com>
2023-07-04 12:09:25 -0300

    Removida URL do AXIOS

--- React/react-base/src/services/axios.js
+++ React/react-base/src/services/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Configurando a URL BASE -> Você não precisara digitar a URL inteira
 export default axios.create({
-  baseURL: "http://34.95.191.245:81/",
+  baseURL: "COLOQUE_SUA_URL_AQUI",
 });
